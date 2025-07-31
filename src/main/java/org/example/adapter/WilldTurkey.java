cd1fb590ead0dd67c678e6faf11c6fbdf1ff1e86
Mohamed-hanfy <mohamed.hanfy.dev@outlook.com>
2025-07-31 23:16:40 +0300

    elbaat lasem e3eesh

---
+++ src/main/java/org/example/adapter/WilldTurkey.java
@@ -0,0 +1,10 @@
+package org.example.adapter;
+
+public class WilldTurkey implements Turkey {
+    public void gobble() {
+        System.out.println("Gobble gobble");
+    }
+    public void fly() {
+        System.out.println("I'm flying a short distance");
+    }
+}
